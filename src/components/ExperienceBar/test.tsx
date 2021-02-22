40459cf1fc7860f821d21c0605ef5a772d688f67
Junior Alves <jjalves96@gmail.com>
2021-02-22 17:35:14 -0300

    feat: create ExperienceBar component

---
+++ src/components/ExperienceBar/test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+
+import ExperienceBar from '.';
+
+describe('<ExperienceBar />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<ExperienceBar />);
+
+    expect(
+      screen.getByRole('heading', { name: /ExperienceBar/i })
+    ).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
