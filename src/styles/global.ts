b30c32a206cc96806ca714dacae930fb2b319a18
Junior Alves <jjalves96@gmail.com>
2021-02-23 17:27:11 -0300

    chore: add new rules into global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,72 +1,6 @@
 import { createGlobalStyle, css } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-  /* ========= Montserrat ========= */
-  @font-face {
-    font-family: 'Montserrat';
-    font-style: normal;
-    font-weight: 300;
-    font-display: swap;
-    src: local(''),
-        url('/fonts/montserrat-v15-latin-300.woff2') format('woff2');
-  }
-
-  @font-face {
-    font-family: 'Montserrat';
-    font-style: normal;
-    font-weight: 400;
-    font-display: swap;
-    src: local(''),
-        url('/fonts/montserrat-v15-latin-regular.woff2') format('woff2');
-  }
-
-  @font-face {
-    font-family: 'Montserrat';
-    font-style: normal;
-    font-weight: 600;
-    font-display: swap;
-    src: local(''),
-        url('/fonts/montserrat-v15-latin-600.woff2') format('woff2');
-  }
-
-  /* ========= Inter ========= */
-  @font-face {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 400;
-    font-display: swap;
-    src: local(''),
-        url('/fonts/inter-v3-latin-regular.woff2') format('woff2'),
-  }
-
-  @font-face {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 500;
-    font-display: swap;
-    src: local(''),
-        url('/fonts/inter-v3-latin-500.woff2') format('woff2'),
-  }
-
-  @font-face {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 600;
-    font-display: swap;
-    src: local(''),
-        url('/fonts/inter-v3-latin-600.woff2') format('woff2'),
-  }
-
-  /* ========= Rajdhani =========*/
-  @font-face {
-    font-family: 'Rajdhani';
-    font-style: normal;
-    font-weight: 600;
-    font-display: swap;
-    src: local(''),
-        url('/fonts/rajdhani-v10-latin-600.woff2') format('woff2'),
-  }
-
   * {
     margin: 0;
     padding: 0;
@@ -94,7 +28,10 @@ const GlobalStyles = createGlobalStyle`
     }
 
     body {
-      font-family: ${theme.font.family};
+      font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+        Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+        sans-serif;
+      font-display: swap;
       font-size: ${theme.font.sizes.medium};
       background-color: ${theme.colors.background};
       color: ${theme.colors.text};
