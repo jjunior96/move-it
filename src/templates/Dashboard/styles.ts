59b7e215cec9a773785ce875411daca5a6e5a4a5
Junior Alves <jjalves96@gmail.com>
2021-02-23 17:26:36 -0300

    chore: import Countdown component

--- src/templates/Dashboard/styles.ts
+++ src/templates/Dashboard/styles.ts
@@ -1,3 +1,55 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  max-width: 960px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  padding: 2.4rem 2rem;
+  margin: 0 auto;
+`;
+
+export const Content = styled.section`
+  flex: 1;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 10rem;
+  align-content: center;
+`;
+
+export const ContentLeft = styled.div``;
+
+export const Line = styled.div`
+  width: 100%;
+  margin: 5.6rem 0;
+
+  &:last-child {
+    margin-top: 5.6rem;
+    margin-bottom: 0;
+  }
+`;
+
+export const ContentRight = styled.div`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.white};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-radius: 6px;
+    box-shadow: ${theme.box.shadow};
+  `}
+`;
+
+export const ContentRightTitle = styled.p`
+  ${({ theme }) => css`
+    color: ${theme.colors.text};
+    font-size: ${theme.font.sizes.xxlarge};
+    line-height: 3rem;
+    max-width: 25rem;
+    text-align: center;
+  `}
+`;
+
+export const ContentRightText = styled.div``;
