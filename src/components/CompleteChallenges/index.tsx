1a95096b20dbbf8ad366e2e9c096ffe33de55311
Junior Alves <jjalves96@gmail.com>
2021-02-24 15:55:21 -0300

    chore: import Context

--- src/components/CompleteChallenges/index.tsx
+++ src/components/CompleteChallenges/index.tsx
@@ -1,11 +1,15 @@
+import { ChallengesContext } from 'contexts/ChallengesContext';
+import { useContext } from 'react';
 import * as S from './styles';
 
 const CompleteChallenges = () => {
+  const { challengesCompleted } = useContext(ChallengesContext);
+
   return (
     <S.Container>
       <S.Info>
         <S.TitleChallenges>Desafios completos</S.TitleChallenges>
-        <S.NumberChallenges>00</S.NumberChallenges>
+        <S.NumberChallenges>{challengesCompleted}</S.NumberChallenges>
       </S.Info>
       <S.BarChallenges />
     </S.Container>
